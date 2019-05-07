55a374833e41df8b46698b1ccfb1f0c0d8ed9583
johannes ae <jojo.ae@hotmail.de>
2019-05-07 20:52:05 +0200

    restructure into all functions, basic framework for analysis and writing

--- anls/try1/tp_anls.py
+++ anls/try1/tp_anls.py
@@ -1,11 +1,11 @@
 import json
 import os
+import csv
 
 import numpy as np
 import graph_tool as gt
 from graph_tool.all import *
 from graph_tool import *  
-
 
 
 import sqlite3
@@ -15,86 +15,159 @@ from os import listdir
 
 
 # todos
+
+## need general analysis function 
+## also general output function
+
+
+# done
 # write graphs to gt objects
 
 
-stage_dir = "/media/johannes/D45CF5375CF514C8/Users/johannes/mlhd/0-15/stage/"
-stage_files = listdir(stage_dir)
-
-# gx=load_graph(stage_dir + 'gts/' + '1167606000-1175378400.gt')
 
 
-# for i in stage_files[1:25]:
-#     print(i)
 
-#     gx = gt.load_graph_from_csv(stage_dir+i, directed=True, string_vals=True, csv_options={'delimiter':'\t'})
+def get_mbid_v_dict():
 
-#     gx_name = stage_dir + 'gts/' + i[0:21] + ".gt"
+    vcnt = len(list(gx.vertices()))
 
-#     gx.save(gx_name)
+    cntr = 0
+    cntr2 = 0
 
-# internal vertex maps should be there, seem to 
+    mbid_v_dict = {}
+    for i in range(0,vcnt):
+        mbid_v_dict[gx.vp.name[i]] = i
 
+        cntr+=1
+        cntr2 +=1
+        if cntr2 == 5000:
+            print(cntr, round(cntr/vcnt,2))
+            cntr2 = 0
 
-# get albums at point in time with playcount > X
-# get tags from sqldb
-# get all tags with weights > Y
-
-
-rel_albs = find_vertex_range(gx, 'in', (300, 10**10))
-rel_mbids = [gx.vp.name[i] for i in rel_albs]
-
-
-tag_sqldb="/home/johannes/Dropbox/gsss/thesis/anls/try1/add_data/alb_tags1.sqlite"
-conn = sqlite3.connect(tag_sqldb)
-c=conn.cursor()
-
-rel_mbids_sub = rel_mbids[0:10]
+    return(mbid_v_dict)
 
 
-sql = "select mbid, tag, weight from tags where mbid in ({seq})".format(
-    seq=','.join(['?']*len(rel_mbids_sub)))
 
-stags = c.execute(sql, rel_mbids_sub).fetchall()
 
-# sql2 = "select distinct tag from tags where weight > 10 and mbid in ({seq})".format(
+def get_mbids(min_in_deg):
+    rel_albs = find_vertex_range(gx, 'in', (min_in_deg, 10**10))
+    rel_mbids = [gx.vp.name[i] for i in rel_albs]
+    return(rel_mbids)
+
+
+def sqlite_setup():
+    tag_sqldb="/home/johannes/Dropbox/gsss/thesis/anls/try1/add_data/alb_tags1.sqlite"
+    conn = sqlite3.connect(tag_sqldb)
+    c=conn.cursor()
+    return(c)
+
+# rel_mbids_sub = rel_mbids[0:10]
+
+
+# sql = "select mbid, tag, weight from tags where mbid in ({seq})".format(
 #     seq=','.join(['?']*len(rel_mbids_sub)))
 
-sql2 = "select distinct tag from tags where weight > 10 group by tag having count(*) > 100"
+# stags = c.execute(sql, rel_mbids_sub).fetchall()
 
-unq_tags = c.execute(sql2).fetchall()
-ut_dict = {}
-for i in unq_tags:
-    ut_dict[i[0]] = 0
+def get_unq_tags(rel_mbids, min_weight, min_cnt):
 
-# need sql call for each tag i think
+    sql2 = "select distinct tag from tags where weight > " + str(min_weight) + " and mbid in ({seq})".format(
+        seq=','.join(['?']*len(rel_mbids)))
+    
+    sql2 = sql2 + " group by tag having count (*) > " + str(min_cnt)
 
-utx = unq_tags[0][0]
+    unq_tags = c.execute(sql2, rel_mbids).fetchall()
+    unq_tags = [i[0] for i in unq_tags]
+    return(unq_tags)
+
+
+# ut_dict = {}
+# for i in unq_tags:
+#     ut_dict[i[0]] = 0
+
+
+def get_tag_mbid_dict(rel_mbids, unq_tags, min_weight):
+    sql = "select mbid, tag, weight from tags where weight > 10 and mbid in ({seq})".format(
+        seq=','.join(['?']*len(rel_mbids)))
+
+    sql = sql + " and tag in ({seq})".format(
+        seq=','.join(['?']*len(unq_tags)))
+
+    daet_ttl = c.execute(sql, rel_mbids+unq_tags).fetchall()
+
+    tag_mbid_dict ={}
+
+    for i in daet_ttl:
+        if i[1] in tag_mbid_dict.keys():
+            tag_mbid_dict[i[1]].append((i[0], i[2]))
+        else:
+            tag_mbid_dict[i[1]] = [(i[0], i[2])]
+
+    return(tag_mbid_dict)
+
+
 
 def get_weighted_plcnt(utx):
 
-    # qry_lst = list(rel_mbids_sub)
-    qry_lst = list(rel_mbids)
-    qry_lst.append(utx)
+    tag_rel_mbids = [i[0] for i in tag_mbid_dict[utx]]
+    tag_rel_weits = [i[1]/100 for i in tag_mbid_dict[utx]]
 
-    sql3 = "select mbid, weight from tags where weight > 10 and mbid in ({seq}) and tag=?".format(
-        seq=','.join(['?']*len(rel_mbids)))
+    tag_rel_v_ids = [mbid_v_dict[i] for i in tag_rel_mbids]
+    tag_rel_v_dgs = [gx.vertex(i).in_degree() for i in tag_rel_v_ids]
 
-    sql3 = "select mbid, weight from tags where weight > 10 and tag=?"
-
-    daet_utx = c.execute(sql3, (utx,)).fetchall()
-    # something wrong with sql, maybe need to filter afterwards? 
-
-    # tag weights
-    tag_rel_mbids = [i[0] for i in daet_utx]
-    tag_rel_weits = [i[1]/100 for i in daet_utx]
-    
-    tag_get_ids = [mbid_v_dict[i] for i in tag_rel_mbids]
-    tag_rel_dgs = [gx.vertex(i).in_degree() for i in tag_get_ids]
-
-    tag_plcnt = sum([a*b for a,b in zip(tag_rel_weits,tag_rel_dgs)])
+    tag_plcnt = sum([a*b for a,b in zip(tag_rel_weits,tag_rel_v_dgs)])
 
     return(tag_plcnt)
+
+
+def res_wr(t, tag, res, outfile):
+    res_str = [str(i) for i in res]
+    prntrow = [t, tag] + res_str
+    
+    with open(outfile, 'a') as fo:
+        wr = csv.writer(fo)
+        wr.writerow(prntrow)
+
+
+
+gt_dir = "/media/johannes/D45CF5375CF514C8/Users/johannes/mlhd/0-15/stage/gts/"
+gt_files = listdir(gt_dir)
+
+
+for i in gt_files:
+    print('load graph')
+    tp = i[0:21]
+    gx=load_graph(gt_dir + i)
+
+    outfile= "/home/johannes/Dropbox/gsss/thesis/anls/try1/results/t1.csv"    
+
+    print('creating mbid vertex index')
+    mbid_v_dict = get_mbid_v_dict()
+
+    print('get relevant mbids')
+    rel_mbids = get_mbids(150)
+    c = sqlite_setup()
+
+    print('get relevant tags')
+    unq_tags = get_unq_tags(rel_mbids, 10, 10)
+
+    print('build tag mbid dict')
+    tag_mbid_dict = get_tag_mbid_dict(rel_mbids, unq_tags, 10)
+    
+    print ('analyze')
+    for utx in unq_tags:
+        tag_plcnt = get_weighted_plcnt(utx)
+        res = [tag_plcnt]
+
+        res_wr(tp, utx, res, outfile)
+
+        print(utx, tag_plcnt)
+
+
+
+
+## need dict of mbids to vertex index
+## vertex count: where was it? what was it important for? density, but apparently not
 
 
 
@@ -103,40 +176,40 @@ def get_weighted_plcnt(utx):
 # not retrieving tags doesn't improve really anything
 
 
-for utx in unq_tags:
-    print(utx, ii)
-    ut_dict[utx[0]] = get_weighted_plcnt(utx[0])
 
 
-ut_dict[utx] = tag_plcnt
+# differet number of tags because i can't really filter out the tags i don't want?
+# could add requirement of being in unq tags
+# then a bunch of tags are missing
+# comes from overly permissive unique tag call: should be limited to mbids
 
-## need dict of mbids to vertex index
-## vertex count: where was it? what was it important for? density, but apparently not
-##
 
-
-vcnt = len(list(gx.vertices()))
-
-cntr = 0
-cntr2 = 0
-
-mbid_v_dict = {}
-for i in range(0,vcnt):
-    mbid_v_dict[gx.vp.name[i]] = i
-
-    cntr+=1
-    cntr2 +=1
-    if cntr2 == 1000:
-        print(cntr)
-        cntr2 = 0
+# diffx = set(unq_tags2) - set(tag_mbid_dict.keys())
 
 
 
+# unq_tags2 = [i[0] for i in unq_tags]
+
+# daet_ttl = c.execute(sql, rel_mbids+unq_tags2).fetchall()
+# # sql requests seem to be taking almost the same time regardless of size?
+
+# mbid_tag_dict = {}
+# cntr =0
+# cntr2 = 0
+
+# for i in daet_ttl:
+#     if i[0] in mbid_tag_dict.keys():
+#         mbid_tag_dict[i[0]].append((i[1], i[2]))
+#     else:
+#         mbid_tag_dict[i[0]] = [(i[1], i[2])]
+
+#     cntr+=1
+#     cntr2 +=1
+#     if cntr2 == 1000:
+#         print(cntr)
+#         cntr2 = 0
 
 
-
-
-
-
+# ## need other way you moron
 
 
