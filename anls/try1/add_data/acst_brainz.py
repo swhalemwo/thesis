eb9c942cf4a0a60abc4b3b2d8ca7d936f0056e9f
johannes ae <jojo.ae@hotmail.de>
2019-05-25 18:04:48 +0200

    start with musicbrainz data retrieval

--- anls/try1/add_data/acst_brainz.py
+++ anls/try1/add_data/acst_brainz.py
@@ -2,7 +2,7 @@ from clickhouse_driver import Client
 import string
 import random
 from collections import Counter
-
+import json
 
 
 # need to get frequency for all the songs
@@ -74,4 +74,72 @@ client.execute('create table song_info3 (mbid String, abbrv String, rndm Int32, 
 client.execute("""insert into song_info3 select * from song_info join
 (select song as abbrv, cnt from song_info2) using (abbrv)""")
 
+
+##########################
+# retrieving information #
+##########################
+
+url = "https://acousticbrainz.org/api/v1/04fa3746-035a-48f1-86b2-3514940c7aaf/high-level"
+
+
+url="https://acousticbrainz.org/api/v1/high-level?recording_ids=96685213-a25c-4678-9a13-abd9ec81cf35"
+
+url="https://acousticbrainz.org/api/v1/high-level?recording_ids=c69310f9-e2e5-4fb2-ac26-836913c478d4"
+
+
+top_mbids=client.execute('select mbid from song_info3 order by cnt desc limit 20')
+
+import urllib.request
+
+for i in top_mbids:
+    url = 'https://acousticbrainz.org/api/v1/' + i[0] + '/low-level/'
+    url = 'https://acousticbrainz.org/api/v1/low-level?recording_ids='+ i 
+    
+    with urllib.request.urlopen(url) as url2:
+        data = json.loads(url2.read().decode())
+
+data[i]['0']['metadata']['audio_properties']['length']
+
+
+
+data[i]['0']['highlevel'].keys()
+data[i]['0']['highlevel']['timbre']['all']['bright']
+
+
+6a95c356-2287-417f-a2a7-99bfb3730321
+
+
+
+danceability
+gender
+genre_dortmund
+genre_electronic
+genre_rosamerica
+genre_tzanetakis
+ismir04_rhythm
+mood_acoustic
+mood_aggressive
+mood_electronic
+mood_happy
+mood_party
+mood_relaxed
+mood_sad
+moods_mirex
+timbre
+tonal_atonal
+voice_instrumental
+
+Piazzai:
+length,
+danceability
+main key
+scale
+frequency of main key
+chord progression
+scale of chord key
+bpm
+total count of beats
+
+could make separate tables with song - album - artist
+if i want to use some more industry-level explanatory mechanisms
 
