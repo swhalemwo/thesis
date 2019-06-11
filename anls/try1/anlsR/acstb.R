22de7a2aea6753cb1927c4294b74ed2f78ac9848
johannes ae <jojo.ae@hotmail.de>
2019-06-11 18:12:32 +0200

    improve functions for individual niche volume distribution

--- anls/try1/anlsR/acstb.R
+++ anls/try1/anlsR/acstb.R
@@ -1,4 +1,5 @@
 library(PerformanceAnalytics)
+library(reshape2)
 
 ## * inspect variable distributions
 
@@ -114,72 +115,16 @@ qryer <- function(gnr){
     return(df.tags2)
 }
 
-## library(raster)
-library(radiant.data)
-
-df.tags <- qryer(gnr)
-
-
-volumer <- function(df.tags, gnr){
-    ## df[,gnr] <- 0
-    ## df[,gnr][which(df$id %in% df.tags$mbid)] <- 1
-    ## df[,gnr][which(df$id %in% df.tags$mbid)] <- df.tags2$rel_weight
-
-    dfx <- merge(df, df.tags, by.x ='id', by.y='mbid', all=TRUE)
-    
-    dims = c("dncblt", "gender", "timb_brt", "tonal", "voice", 'mood_acoustic', 'mood_aggressive', 'mood_electronic', 'mood_happy', 'mood_party', 'mood_relaxed', 'mood_sad')
-
-    ttl_vol = c()
-    tops <- c()
-    bots <- c()
-        
-    for (dimx in dims){
-        meanx <- mean(dfx[which(dfx$tag == gnr),dimx])
-        cnts <- dfx[which(dfx$tag==gnr),'cnt']
-        rweits <- dfx[which(dfx$tag==gnr),'rel_weight']
-
-        ## hm those with high playcounts have low relweights, those with high relweights have low cnts
-        ## makes sense tbh
-        ## cbnd <- cnts * rweits
-        ## cbnd <- sqrt(cnts) * rweits
-
-        corx <- cor(cnts, rweits)
-        ## cbnd correlates stronger with cnts than rweits
-
-        w.meanx  <- weighted.mean(dfx[which(dfx$tag==gnr),dimx], rweits)
-        ## need to decide what to use
-        ## unweighted seems to be a bit bigger in ~75% of cases
-        ## print(c(meanx, w.meanx))
-        meanx <- w.meanx
-        
-        ## lel basically same as non weighted mean
-        ## is theoretical question: what defines a genre: is it a song that is played a lot, but only has few tags?
-        ## or is it a song that isn't played a lot but is very strongly associated with it?
-        ## hmm would say second one, rweits
-        ## maybe sqrt(cnts)
-
-        sdx = sd(df[which(dfx$tag==gnr),dimx])
-        w.sdx <- weighted.sd(dfx[which(dfx$tag==gnr),dimx], rweits)
-        ## print(c(sdx, w.sdx))
-
-        ## need to decide what to use
-        sdx <- w.sdx
-            
-        top = meanx + 1.5*sdx
-        bot = meanx - 1.5*sdx
-
-        tops <- c(tops, top)
-        bots <- c(bots, bot)
-
-        rngx = top-bot
-        ttl_vol <- c(ttl_vol, rngx)
-    }
-
-    pruct = prod(ttl_vol)
-
-    print(c(gnr, round(pruct,3)))
-
-    ## see how many other songs are in niche
+niche.fullns <- function(dfx, dims, topx, bots) {
+    ## calculates the number of songs in niche
+    ## i think i can do distinctiveness here
+    ## average of distances to N styles
+    ## how to get distance
+    ## how to set other styles when I don't have cohort
+    ## use niche?: loop over songs in niche, bin their genres (based on cnt, rweit, both),
+    ## then check which bins are "full enough"
+    ## 
+ 
     dfx2 <- dfx
 
     for (i in dims){
@@ -190,15 +135,146 @@ volumer <- function(df.tags, gnr){
         dfx2 <- dfx2[which(dfx2[,i] > botx & dfx2[,i] < topx),]
         
     }
-    print(c(dim(df.tags)[1],dim(dfx2)[1], dim(df.tags)[1]/dim(dfx2)[1]))
+    return(c(nrow(df.tags),nrow(dfx2), nrow(df.tags)/nrow(dfx2)))
     ## NEED TO GET playcount of all songs
 }
 
+## library(raster)
+library(radiant.data)
+
+df.tags <- qryer(gnr)
+
+weigher <- function(dfx, gnr, dims, wts, cutoffs){
+
+    res.ttl <- list()
+
+    ## res <- c(1,2,3)
+    ## ttl_res[[length(ttl_res)+1]] <- res
+    ## names(ttl_res[[length(ttl_res)]]) <- 'stuff'
+    
+    for (ctf in cutoffs){
+
+        ttl_vol = c()
+        tops <- c()
+        bots <- c()
+
+        ctf.ids <- which(dfx[which(dfx$tag == gnr),'rel_weight'] > ctf)
+
+        for (dimx in dims){
+            meanx <- mean(dfx[which(dfx$tag == gnr),dimx])
+
+            ## plot(cnts, rweits)
+            
+            ## hm those with high playcounts have low relweights, those with high relweights have low cnts
+            ## makes sense tbh
+            ## cbnd <- cnts * rweits
+            ## cbnd <- sqrt(cnts) * rweits
+            ## cbnd correlates stronger with cnts than rweits
+
+            meanx  <- weighted.mean(dfx[which(dfx$tag==gnr),dimx][ctf.ids], wts[ctf.ids])
+            ## need to decide what to use
+            ## unweighted seems to be a bit bigger in ~75% of cases
+            ## print(c(meanx, w.meanx))
+            
+            ## lel basically same as non weighted mean
+            ## is theoretical question: what defines a genre: is it a song that is played a lot, but only has few tags?
+            ## or is it a song that isn't played a lot but is very strongly associated with it?
+            ## hmm would say second one, rweits
+            ## maybe sqrt(cnts)
+
+            sdx <- weighted.sd(dfx[which(dfx$tag==gnr),dimx][ctf.ids], wts[ctf.ids])
+            ## print(c(sdx, w.sdx))
+
+            top = meanx + 1.5*sdx
+            bot = meanx - 1.5*sdx
+
+            tops <- c(tops, top)
+            bots <- c(bots, bot)
+
+            rngx = top-bot
+            ttl_vol <- c(ttl_vol, rngx)
+        }
+        pruct = prod(ttl_vol)
+
+        res.cf <- list(pruct, tops, bots, length(ctf.ids), sum(wts[ctf.ids]))
+        names(res.cf) <- c('vol', 'tops', 'bots', 'nbr', 'wts.sum')
+
+        res.ttl[[length(res.ttl)+1]] <- res.cf
+    }
+
+    names(res.ttl) <- cutoffs
+    return(res.ttl)
+}
+
+
+volumer <- function(df.tags, gnr){
+    ## df[,gnr] <- 0
+    ## df[,gnr][which(df$id %in% df.tags$mbid)] <- 1
+    ## df[,gnr][which(df$id %in% df.tags$mbid)] <- df.tags2$rel_weight
+
+    dfx <- merge(df, df.tags, by.x ='id', by.y='mbid', all=TRUE)
+    
+    ## dims = c("dncblt", "gender", "timb_brt", "tonal", "voice", 'mood_acoustic', 'mood_aggressive', 'mood_electronic', 'mood_happy', 'mood_party', 'mood_relaxed', 'mood_sad')
+
+    dims = c("dncblt", "gender", "timb_brt", "tonal", "voice")
+
+    cnts <- dfx[which(dfx$tag==gnr),'cnt']
+    rweits <- dfx[which(dfx$tag==gnr),'rel_weight']
+
+    ## print(cnts)
+    ## print(dim(df.tags))
+    ## corx <- cor(cnts, rweits)
+
+    ## hm actually only need the df.tags songs here
+    ## wonder if it's much faster that way? idfk
+
+    ## cutoffs <- c(0, 0.2, 0.4, 0.6)
+    cutoffs <- seq(0, 1, 0.01)
+
+    res.cnts <- weigher(dfx, gnr, dims, cnts, cutoffs)
+
+    ## res.cnts <- weigher(dfx, dims, rweits, cutoffs)
+    ## res.cnts <- weigher(dfx, dims, rep(1,nrow(df.tags)), cutoffs)
+    ## weighing with rweits doesn't make much sense since cutoffs are now based on it    
+
+    vol.dbt <- unlist(lapply(res.cnts, function(x){x['vol']}))
+    ## print(vol.dbt)
+    print(max(vol.dbt, na.rm=TRUE))
+
+    vol.dbt.nrmlzd <- vol.dbt/max(vol.dbt, na.rm=TRUE)
+    vol.line <- as.data.frame(cbind(cutoffs, vol.dbt.nrmlzd, rep(gnr, length(cutoffs))))
+    return(vol.line)
+    
+    ## res.rweits <- weigher(dfx, dims, rweits, cutoffs)
+
+    ## weigh by cnts, then calculate space for each subgroup?
+    ## or rather other way around: for each subgroup of weight ranges, weigh by count!
+}
+
+
+
+vol.line.df <- data.frame(matrix(ncol=3, nrow=0))
+names(vol.line.df) <- c("cutoffs","vol.dbt.nrmlzd", "V3")
+
+
+        
+    ## see how many other songs are in niche
 
 for (i in gnrs){
+    print(i)
     df.tags = qryer(i)
-    volumer(df.tags, i)
+    vol.linex <- volumer(df.tags, i)
+    vol.line.df <- rbind(vol.line.df, vol.linex)
 }
+
+vol.line.df$cutoffs <- as.numeric(as.character(vol.line.df$cutoffs))
+vol.line.df$vol.dbt.nrmlzd <- as.numeric(as.character(vol.line.df$vol.dbt.nrmlzd))
+
+ggplot(vol.line.df, aes(x=cutoffs, y=vol.dbt.nrmlzd, group=V3)) +
+    geom_line(aes(color=V3)) +
+    scale_color_brewer(palette="Dark2")
+
+
 ## neato
 ## add: weighted mean/sd?
 ## weigh by
@@ -230,7 +306,6 @@ ggplot(df, aes(x=dncblt, y=tonal)) +
 ggplot(df[which(df$pop_rock==1),], aes(x=dncblt, y=tonal)) +
     geom_jitter(width=0.05, height=0.05) + 
     geom_rect(mapping=aes(xmin=0.25, xmax=0.5, ymin=0.25, ymax=0.5), alpha=0.005)
-
 
 
 gnr = 'pop_rock'
