93d4667aaf84ad73b7fdb81630d1c493297a8c72
johannes ae <jojo.ae@hotmail.de>
2019-06-19 11:23:16 +0200

    fix dirs

--- anls/try1/pre_proc/ch_bb.py
+++ anls/try1/pre_proc/ch_bb.py
@@ -9,10 +9,11 @@ paths= [
     '/media/johannes/D45CF5375CF514C8/Users/johannes/mlhd/0-15/03/',
     '/media/johannes/D45CF5375CF514C8/Users/johannes/mlhd/0-15/04/',
     '/media/johannes/D45CF5375CF514C8/Users/johannes/mlhd/0-15/05/',
-    '/home/johannes/Downloads/mlhd/06',
-    '/home/johannes/Downloads/mlhd/07',
-    '/home/johannes/Downloads/mlhd/08',
-    '/home/johannes/Downloads/mlhd/09']
+    '/home/johannes/Downloads/mlhd/06/',
+    '/home/johannes/Downloads/mlhd/07/',
+    '/home/johannes/Downloads/mlhd/08/',
+    '/home/johannes/Downloads/mlhd/09/']
 
 for p in paths:
+    print(p)
     os.system('python3.6 ~/Dropbox/gsss/thesis/anls/try1/pre_proc/read_in_ch_blk.py ' + p)
