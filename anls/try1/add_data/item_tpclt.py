0db4fb06a53b73bd8e5f2a74729a50f851cb5425
johannes ae <jojo.ae@hotmail.de>
2019-06-27 18:57:34 +0200

    funcionify dict creation, experiment with mahalanobis dist

--- anls/try1/add_data/item_tpclt.py
+++ anls/try1/add_data/item_tpclt.py
@@ -6,6 +6,7 @@ import numpy as np
 import math
 import time
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import mahalanobis
 
 def weighted_avg_and_std(values, weights):
     """
@@ -25,6 +26,7 @@ vrbls = ['dncblt','gender','timb_brt','tonal','voice']
 
 def get_dfs(vrbls, min_cnt, min_weight, min_rel_weight, min_tag_aprnc):
     # still has to be adopted to be able to accommodate time slices
+    # wonder if the subsequent sorting can result in violations again?
     
     """retrieves acoustic data (vrbls) and tags of corresponding songs
         queries are written to only retrieve only complete matches (songs for which both musicological data and tags are available
@@ -43,6 +45,7 @@ def get_dfs(vrbls, min_cnt, min_weight, min_rel_weight, min_tag_aprnc):
     # min_cnt = 400
 
     # generate string for musicological data
+    
     client_str = """
     SELECT lfm_id as mbid,""" +  vrbl_strs + """
     FROM acstb
@@ -118,72 +121,34 @@ min_cnt = 200
 min_weight = 10
 min_rel_weight = 0.1
 min_tag_aprnc = 20
-min_cnt = 200
 
-
-df_ac, df_tg = get_dfs(vrbls, min_cnt, min_weight, min_rel_weight, min_tag_aprnc)
+df_acst, df_tags = get_dfs(vrbls, min_cnt, min_weight, min_rel_weight, min_tag_aprnc)
 # seems to be working
 
+# songs_acst = df_ac['lfm_id']
+# songs_tags = df_tg['lfm_id']
+# songs_tbp = list(set(songs_acst) - set(songs_tags))
 
-
-
-
-
-
-    
-
-
-
-    # make it into one function, they are mutually conditioned anyways
-
-
-# rows_tags = client.execute("""select mbid, tag, rel_weight, cnt from
-# (select * from 
-#     (select * from tag_sums
-#         join (select tag, count(*) as tag_ap from tag_sums where weight > 10 and rel_weight > 0.05
-#             group by tag having tag_ap > 10) using tag)
-#     join (select lfm_id as mbid from acstb) using mbid)
-# join (select mbid, cnt from song_info3 where cnt > 400 ) using mbid  
-# where weight > 10 and rel_weight > 0.05""")
-
-
-
-
-songs_tbp = list(set(songs_acst) - set(songs_tags))
-
-df_acst_dict = {}
-c = 0
-for r in df_acst.itertuples():
-    df_acst_dict[r.lfm_id] = c
-    c +=1
-
-# rows to pop
-rstp = []
-for i in songs_tbp:
-    # shouldn't every songs_tbp in df_acst???
-    # yup: songs_tbd are those that are in df_acst but not in df_tags
-    # which means they are in df_acst
-    rtp = df_acst_dict[i]
-    rstp.append(rtp)
-
-df_acst2 = df_acst.drop(rstp)
-# finally works
-
+def dict_gnrgs(df_acst, df_tags):
+    """generates genre song dict and acoustic position dict"""
 # make position dicts because dicts good
 
-gnr_song_dict = {}
-for r in df_tags.itertuples():
-    gnr = r.tag
-    
-    if gnr in gnr_song_dict.keys():
-        gnr_song_dict[gnr].append(r.lfm_id)
-    else:
-        gnr_song_dict[gnr] = [r.lfm_id]
-        
+    gnr_song_dict = {}
+    for r in df_tags.itertuples():
+        gnr = r.tag
 
-acst_pos_dict = {}
-for r in df_acst2.itertuples():
-    acst_pos_dict[r.lfm_id] = r.Index
+        if gnr in gnr_song_dict.keys():
+            gnr_song_dict[gnr].append(r.lfm_id)
+        else:
+            gnr_song_dict[gnr] = [r.lfm_id]
+
+    acst_pos_dict = {}
+    for r in df_acst.itertuples():
+        acst_pos_dict[r.lfm_id] = r.Index
+
+    return(gnr_song_dict, acst_pos_dict)
+
+gnr_song_dict, acst_pos_dict = dict_gnrgs(df_acst, df_tags)
 
 
 unq_tags = list(np.unique(df_tags['tag']))
@@ -195,11 +160,10 @@ df_gnr_acst = df_acst.loc[gnr_acst_ids]
 
 df_gnr_cbmd = pd.merge(df_gnr_tags, df_gnr_acst, on='lfm_id')
 
-trackx = '047a94b1-c42e-4867-8aa5-36d8c1f57d00'
-trackx_loc = list(df_gnr_cbmd['lfm_id']).index(trackx)
+tx = '047a94b1-c42e-4867-8aa5-36d8c1f57d00'
+tx_loc = list(df_gnr_cbmd['lfm_id']).index(trackx)
 
 vrbls = ['dncblt','gender', 'timb_brt','tonal', 'voice']
-
 
 
 # still not clear about how to weigh, whether to use weight or count arrrrr
@@ -221,9 +185,67 @@ aweits = df_gnr_cbmd['cnt']
 cov_mat = np.cov(mat, rowvar=0, aweights=aweits)
 cov_mat_inv = np.linalg.inv(cov_mat)
 
-
-from scipy.spatial.distance import mahalanobis
 mahalanobis(slx, ovrl, cov_mat_inv)
+
+df_gnr_cbmd['mah_dist']=0.0
+
+unq_trks = list(df_gnr_cbmd['lfm_id'])
+
+
+c = 0
+for tx in unq_trks:
+    tx_vls = df_gnr_cbmd[df_gnr_cbmd['lfm_id'] == tx][vrbls]
+    mah_dist = mahalanobis(tx_vls, ovrl, cov_mat_inv)
+
+    df_gnr_cbmd.at[c, 'mah_dist'] = mah_dist
+    c+=1
+
+m_dists = df_gnr_cbmd['mah_dist']
+plt.hist(m_dists, bins='auto')
+plt.show()
+
+# ** why is this so high? need to test with normally distributed data
+# could be due to binominal distribution..
+# but still, that really no one is close is weird
+
+
+rnd1 = np.random.normal(loc=3, scale=0.5, size = 1000)
+rnd2 = np.random.normal(loc=2, scale=2, size = 1000)
+rnd3 = np.random.normal(loc=4, scale=1, size = 1000)
+
+rnd_ar = np.array([rnd1, rnd2, rnd3]).transpose()
+
+cov_mat2 =np.cov(rnd_ar, rowvar=0)
+cm_inv2 = np.linalg.inv(cov_mat2)
+
+df_rnd = pd.DataFrame(rnd_ar, columns = ['r1', 'r2', 'r3'])
+
+means = [np.mean(i) for i in [rnd1, rnd2, rnd3]]
+
+mah_dists = []
+for i in range(1000):
+    distx = mahalanobis(rnd_ar[i,], means, cm_inv2)
+    mah_dists.append(distx)
+# hm still not many at 0, but much closer (min 0.1 vs ~0.8)
+
+
+## ** test with entire data as basis for cov mat
+
+mat = df_acst[vrbls]
+
+cov_mat = np.cov(mat, rowvar=0)
+cov_mat_inv = np.linalg.inv(cov_mat)
+
+
+c = 0
+for tx in unq_trks:
+    tx_vls = df_gnr_cbmd[df_gnr_cbmd['lfm_id'] == tx][vrbls]
+    mah_dist = mahalanobis(tx_vls, ovrl, cov_mat_inv)
+
+    df_gnr_cbmd.at[c, 'mah_dist'] = mah_dist
+    c+=1
+
+    # need to get playcount into weights 
 
 
 
@@ -280,3 +302,34 @@ mahalanobis(slx, ovrl, cov_mat_inv)
     # join ( select tag,count(*) as tag_ap from tag_sums where weight > 10)"""
     # rows_acst = client.execute("""select lfm_id, dncblt,gender,timb_brt,tonal,voice from acstb 
     # join (select mbid as lfm_id, cnt from song_info3 where cnt > 400 ) using lfm_id""")
+
+    
+# rows_tags = client.execute("""select mbid, tag, rel_weight, cnt from
+# (select * from 
+#     (select * from tag_sums
+#         join (select tag, count(*) as tag_ap from tag_sums where weight > 10 and rel_weight > 0.05
+#             group by tag having tag_ap > 10) using tag)
+#     join (select lfm_id as mbid from acstb) using mbid)
+# join (select mbid, cnt from song_info3 where cnt > 400 ) using mbid  
+# where weight > 10 and rel_weight > 0.05""")
+
+
+# popping entries no longer needed
+
+# df_acst_dict = {}
+# c = 0
+# for r in df_acst.itertuples():
+#     df_acst_dict[r.lfm_id] = c
+#     c +=1
+
+# # rows to pop
+# rstp = []
+# for i in songs_tbp:
+#     # shouldn't every songs_tbp in df_acst???
+#     # yup: songs_tbd are those that are in df_acst but not in df_tags
+#     # which means they are in df_acst
+#     rtp = df_acst_dict[i]
+#     rstp.append(rtp)
+
+# df_acst2 = df_acst.drop(rstp)
+# # finally works
