fa2ee0c856654b4f3fbf1dd77d690fef8bacae1e
johannes ae <jojo.ae@hotmail.de>
2019-07-04 19:33:55 +0200

    experiment with overlap (needs better processing), much cleanup

--- anls/try1/add_data/acst_hier.py
+++ anls/try1/add_data/acst_hier.py
@@ -11,113 +11,86 @@ import matplotlib.pyplot as plt
 from scipy.stats import entropy
 from sklearn import preprocessing
 
-def weighted_avg_and_std(values, weights):
-    """
-    Return the weighted average and standard deviation.
+# * hierarchical relations based on split dimensions
+# functions defined in item_tpclt
 
-    values, weights -- Numpy ndarrays with the same shape.
-    """
-    average = np.average(values, weights=weights)
-    # Fast and numerically precise:
-    variance = np.average((values-average)**2, weights=weights)
-    return (average, math.sqrt(variance))
-
-
-client = Client(host='localhost', password='anudora', database='frrl')
-
-avbl_vars = ['id', 'dncblt', 'gender', 'timb_brt', 'tonal', 'voice', 'mood_acoustic', 'mood_aggressive', 'mood_electronic', 'mood_happy', 'mood_party', 'mood_relaxed', 'mood_sad', 'gnr_dm_alternative', 'gnr_dm_blues', 'gnr_dm_electronic', 'gnr_dm_folkcountry', 'gnr_dm_funksoulrnb', 'gnr_dm_jazz', 'gnr_dm_pop', 'gnr_dm_raphiphop', 'gnr_dm_rock', 'gnr_rm_cla', 'gnr_rm_dan', 'gnr_rm_hip', 'gnr_rm_jaz', 'gnr_rm_pop', 'gnr_rm_rhy', 'gnr_rm_roc', 'gnr_rm_spe', 'gnr_tza_blu', 'gnr_tza_cla', 'gnr_tza_cou', 'gnr_tza_dis', 'gnr_tza_hip', 'gnr_tza_jaz', 'gnr_tza_met', 'gnr_tza_pop', 'gnr_tza_reg', 'gnr_tza_roc', 'mirex_Cluster1', 'mirex_Cluster2', 'mirex_Cluster3', 'mirex_Cluster4', 'mirex_Cluster5', 'length', 'label', 'lang', 'rl_type', 'rls_cri']
-
-chsn_vars = ['dncblt', 'gender', 'timb_brt', 'tonal', 'voice']
-
-ids = [avbl_vars.index(i) for i in chsn_vars]
+def vd_fer(g, idx):
+    """creates vertex index dict from graph and id property map"""
+    # vertex_dict
+    vd = {}
+    for i in g.vertices():
+        vd[idx[i]] = int(i)
+    return(vd)
 
 
-rows_acst = client.execute("""select lfm_id, dncblt,gender,timb_brt,tonal,voice from acstb 
-join (select mbid as lfm_id, cnt from song_info3 where cnt > 400 ) using lfm_id""")
+t1 = time.time()
 
-df_acst = pd.DataFrame(rows_acst, columns = ['lfm_id', 'dncblt','gender', 'timb_brt','tonal', 'voice'])
+el_ttl = []
+gnrs = ['metal', 'ambient', 'rock', 'hard rock', 'electronic', 'death metal']
 
-songs_acst = df_acst['lfm_id']
+for gnr in gnrs:
+    # print(gnr)
 
+    # gnr = 'rap'
+    dfc = get_df_cbmd(gnr)
 
-# can make the query longer to select only genres which occur at least X times
-# join statements have to be on different levels i think, that's why i have to make three new lines to take into account the tag appearnace (tag_ap )
+    gnr_cnt = len(dfc)
+    el_gnr = []
+    # print(len(el_gnr))
 
-# have to put another acstb condition in there, which is more restrictive
-# hm now have tags for less than i have acoustic information
-# which makes sense because some for which i have acoustic information fail to meet tag requirements
-# hm could adjust the query
-# idk maybe is easier to pop all that are in acst_df but not in tag_df? 
+    for vrbl in vrbls:
 
-rows_tags = client.execute("""select mbid, tag, rel_weight, cnt from
-(select * from 
-    (select * from tag_sums
-        join (select tag, count(*) as tag_ap from tag_sums where weight > 10 and rel_weight > 0.05
-            group by tag having tag_ap > 10) using tag)
-    join (select lfm_id as mbid from acstb) using mbid)
-join (select mbid, cnt from song_info3 where cnt > 400 ) using mbid  
-where weight > 10 and rel_weight > 0.05""")
+        bins = np.arange(0, 1.1, 0.1)
+        a1, a0 = np.histogram(dfc[vrbl], bins=10)
 
-df_tags = pd.DataFrame(rows_tags, columns=['lfm_id', 'tag', 'rel_weight', 'cnt'])
+        nds1 = [vrbl + str(i) for i in range(1, len(bins))]
+        nds2 = [gnr]*len(nds1)
 
-songs_tags = df_tags['lfm_id']
-len(np.unique(songs_tags))
+        a_wtd = [i/gnr_cnt for i in a1]
 
-# select songs that have to be deleted
-songs_tbp = list(set(songs_acst) - set(songs_tags))
+        elx = [i for i in zip(nds2, nds1, a1, a_wtd)]
 
-# make lfm_id, row_id dict
-df_acst_dict = {}
-c = 0
-for r in df_acst.itertuples():
-    df_acst_dict[r.lfm_id] = c
-    c +=1
+        el_gnr = el_gnr + elx
 
-# rows to pop
-rstp = []
-for i in songs_tbp:
-    # shouldn't every songs_tbp in df_acst???
-    # yup: songs_tbd are those that are in df_acst but not in df_tags
-    # which means they are in df_acst
-    rtp = df_acst_dict[i]
-    rstp.append(rtp)
+    el_ttl = el_ttl + el_gnr
 
-df_acst2 = df_acst.drop(rstp)
-# finally works
+t2 = time.time()
+# not that fast, might be optimized
 
-# make position dicts because dicts good
+# graph acoustic 
+gac = Graph()
 
-gnr_song_dict = {}
-for r in df_tags.itertuples():
-    gnr = r.tag
-    
-    if gnr in gnr_song_dict.keys():
-        gnr_song_dict[gnr].append(r.lfm_id)
-    else:
-        gnr_song_dict[gnr] = [r.lfm_id]
-        
+w = gac.new_edge_property('int16_t')
+w_std = gac.new_edge_property('double')
 
-acst_pos_dict = {}
-for r in df_acst2.itertuples():
-    acst_pos_dict[r.lfm_id] = r.Index
+idx = gac.add_edge_list(el_ttl, hashed=True, string_vals=True, eprops = [w, w_std])
+
+vd = vd_fer(gac, idx)
+
+# needs to be generalized into list for relevant pairs 
+# especially to assess prevalence of non-binary fit and hence possibility of overlap misattribution (lack sub due to area not covered)
+
+vertex_similarity(gac, 'dice', vertex_pairs = [(vd['ambient'],vd['electronic'])])
+# WTF start
+vertex_similarity(gac, 'dice', vertex_pairs = [(vd['ambient'],vd['electronic'])], eweight = w)
+vertex_similarity(gac, 'jaccard', vertex_pairs = [(vd['electronic'],vd['ambient'])], eweight = w)
+# WTFFF end: 
+
+vertex_similarity(gac, 'dice', vertex_pairs = [(vd['ambient'],vd['electronic'])], eweight = w_std)
+vertex_similarity(gac, 'dice', vertex_pairs = [(vd['ambient'],vd['death metal'])], eweight = w_std)
 
 
-unq_tags = list(np.unique(df_tags['tag']))
-# for i unq_tags:
 
-gnr = 'black metal'
-gnr_acst_ids = [acst_pos_dict[i] for i in gnr_song_dict[gnr]]
+# [print(w[gac.edge(vd['ambient'], vd['voice' + str(i)])]) for i in range(1,11)]
+# [print(w[gac.edge(vd['electronic'], vd['voice' + str(i)])]) for i in range(1,11)]
 
-df_gnr_tags = df_tags[df_tags['tag']==gnr]
-df_gnr_acst = df_acst.loc[gnr_acst_ids]
 
-df_gnr_cbmd = pd.merge(df_gnr_tags, df_gnr_acst, on='lfm_id')
+g.vertex(vd['electronic']).out_neighbors()
 
-weighted_avg_and_std(df_gnr_cbmd['dncblt'], df_gnr_cbmd['cnt'])
 
-bins = np.arange(0, 1, 0.1)
-a1, a0 = np.histogram(df_gnr_cbmd['dncblt'], bins=bins)
+# * some old stuff
 
+# ** plotting dists for multiple varibales and genres
 fig, ax = plt.subplots()
 # ax.bar(a0[:-1], a1)
 
@@ -132,28 +105,6 @@ for i in range(len(a1)):
 plt.bar(ax_lbl, a1)
 plt.show()
 
-
-# x1 = np.linspace(0.0, 5.0)
-# x2 = np.linspace(0.0, 2.0)
-
-
-# y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
-# y2 = np.cos(2 * np.pi * x2)
-
-# plt.subplot(3, 1, 1)
-# plt.plot(x1, y1, 'o-')
-# plt.title('A tale of 2 subplots')
-# plt.ylabel('Damped oscillation')
-
-# plt.subplot(3, 1, 2)
-# plt.plot(x2, y2, '.-')
-# plt.xlabel('time (s)')
-# plt.ylabel('Undamped')
-
-# plt.subplot(3, 1, 3)
-# plt.plot(x2, y2, '.-')
-# plt.xlabel('time (s)')
-# plt.ylabel('Undamped')
 
 
 def sb_pltr(ttl, row, nbr, xs, ys, xlbl, ylbl):
@@ -201,101 +152,12 @@ plt.show()
 # g1 = 'black metal'
 # g2 = 'metal'
 
-
 # 371 comparisons in per sec
 # 16 mil is gonna take like 12 hours
 # and it's not even weighted yet
 # 
 
 
-
-
-db = DiscoDB(gnr_song_dict)
-
-# t1 = time.time()
-# for i in range(100):
-#     x = (list(db.query(Q.parse('rock & pop rock'))))
-# t2 = time.time()    
-# 806/sec, not bad
-# performance goes a bit down (738-780) when running multiple processes, oh well, NBD
-
-
-# t1 = time.time()
-# for i in range(500):
-
-#     g1_entrs = gnr_song_dict[g1]
-#     g2_entrs = gnr_song_dict[g2]
-
-#     x = set.intersection(set(g1_entrs), set(g2_entrs))
-#     # print(len(x)/len(g1_entrs))
-
-# t2 = time.time()
-
-def gnr_sub_disco(g1, g2):
-    """compare overlap between two genres
-    outspeeded by metaqueries"""
-    qry1 = g1 + " & " + g2
-    ovlp = len(list(db.query(Q.parse(qry1))))
-    ovlp_prop = ovlp/len(gnr_song_dict[g1])
-    return(ovlp_prop)
-
-
-
-
-
-# [x for _,x in sorted(zip(Y,X))]
-
-tag_cnts = [len(gnr_song_dict[i]) for i in unq_tags]
-
-# get tags sorted by playcount or appearance or something
-tags_srtd=[x for _,x in sorted(zip(tag_cnts, unq_tags), reverse=True)]
-
-
-
-# ovlp_res = []
-
-# for g1 in tags_srt_sub:
-#     g1_ovlps = []
-#     for g2 in tags_srt_sub:
-#         ovlp = gnr_sub_disco(g1, g2)
-#         g1_ovlps.append(ovlp)
-
-#     ovlp_res.append(g1_ovlps)
-
-
-# ovlp_ar = np.array(ovlp_res)
-
-
-# a = np.histogram(ovlp_ar, bins=30)
-
-# a0 = np.append(np.array([0]), a[0])
-# a1 = [i*10 for i in a[1]]
-
-
-# artists seem to be between clusters: rammstein, marilyn manson, korn, disturbed, system of a down, linkin park
-# would suck to exclude them, they've basically become a brand now
-# are part of the cognitive web that people use to classify music
-
-
-
-# plt.bar(a1, a0)
-# plt.show()
-
-
-# plt.hist(a, bins='auto')
-# plt.show()
-
-# just text no vertex so much nicer
-# graphviz dot good: can handle linebreaked titles
-
-
-# can add transitivity violations as variable:
-# see if any there are supersets of supersets that are not supersets of X
-    
-# fuck yeah
-# could also be way of filtering: only relevant genres are those that either have subgenres or are subgenres of something
-
-# see if replication of values at lower levels:
 
 
 
@@ -485,7 +347,6 @@ plt.show()
 
 
 
-
 # * kullback-Leibler divergence
 
 x = [i for i in range(0,10)]
@@ -663,50 +524,5 @@ klbk_lblr_dist(x2,x1)
 
 # * scrap 
 
-
-# df = pd.DataFrame(columns = ['id'] + chsn_vars)
-# df[0] = row[0] + float(row[1]) + float(row[2]) + float(row[3]) + float(row[4]) + float(row[5])
-
-# df.loc[0] = entry
-
-# row_dict = {'id':row[0], 'dncblt':float(row[1]), 'gender':float(row[2]), 'timb_brt':float(row[3]), 'tonal':float(row[4]), 'voice':float(row[5])}
-
-# df = pd.DataFrame([row_dict])
-
-
-
-# entry = [row[0]] + [float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5])]
-
-# df = pd.DataFrame(columns = ['id'] + chsn_vars)
-# dims_file = '/home/johannes/Dropbox/gsss/thesis/anls/try1/add_data/acstbrnz.csv'
-# c = 0
-# row_list = []
-# with open(dims_file, 'r') as fi:
-#     rdr = csv.reader(fi)
-
-#     for row in rdr:
-
-#         row_dict = {'id':row[0], 'dncblt':float(row[1]), 'gender':float(row[2]), 'timb_brt':float(row[3]), 'tonal':float(row[4]), 'voice':float(row[5])}
-#         row_list.append(row_dict)
-
-#         c+=1
-        
-#         # entry = [row[0]] + [float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5])]
-#         # df.loc[c] = entry
-        
-#         # df[c] = row[0:6]
-# dk1 = 0
-# for i in range(0,10):
-#     if x2[i] !=0 and x1[i] != 0:
-#         sub_res = x1[i] * np.log(x1[i]/x2[i])
-#         dk1 = dk1 + sub_res
-#         print(sub_res)
-#     else:
-#         print('lel')
-
-# * older ch queries
-# rows_tags = client.execute("""select mbid, tag, rel_weight, cnt from tag_sums 
-# join (select mbid, cnt from song_info3 where cnt > 400 ) using mbid 
-# where weight > 10 and rel_weight > 0.05""")
 
 
