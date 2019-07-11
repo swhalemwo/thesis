05f2ad162f92a502120c38fb664a95d5ced68e39
johannes ae <jojo.ae@hotmail.de>
2019-07-11 13:06:00 +0200

    more functionalization/automation, error handling not complete in case of loss of connection tho

--- anls/try1/pre_proc/mlhd_dl.py
+++ anls/try1/pre_proc/mlhd_dl.py
@@ -1,5 +1,3 @@
-
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,12 +6,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import time
+import argparse
+import os
+import json
 
-
-
-# driver.get("https://www.google.com")
-
-def setup(aimed_secs):
+def setup(start, end):
     amt2 = "application/x-tar;application/x-gtar"
     profile = FirefoxProfile()
     profile.set_preference("browser.download.panel.shown", False)
@@ -26,18 +23,39 @@ def setup(aimed_secs):
     profile.set_preference("browser.download.manager.showWhenStarting", False)
     profile.set_preference("browser.download.dir", '~/mlhd/dl/')
 
-
-    driver = webdriver.Firefox(firefox_profile=profile)
+    from selenium.webdriver.firefox.options import Options
+    options = Options()
+    # options.headless = True
+    
+    driver = webdriver.Firefox(firefox_profile=profile, options=options)
 
     driver.get('http://bit.ly/MLHD-Dataset')
 
-    # scroll down a bunch
-    actions = ActionChains(driver)
-    for _ in range(8):
-        actions.send_keys(Keys.SPACE).perform()
-        time.sleep(1)
+    print('wait till page loaded')
+    time.sleep(5)
+    
 
-    # get links
+    # scroll down a bunch
+
+
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.TAB).perform()
+
+    while True:
+        actions.send_keys(Keys.SPACE).perform()
+        print('scrolling down a bit')
+        time.sleep(2)
+
+        links = get_links(driver, start, end)
+        if len(links) == len(range(start, end)):
+            break
+
+    return(driver, links)
+
+def get_links(driver, start, end):
+    """get links"""
+    
+    aimed_secs = [i for i in range(start,end)]
     links = {}
     for a in driver.find_elements_by_xpath('.//a'):
         try:
@@ -53,35 +71,8 @@ def setup(aimed_secs):
     return(links)
 
 
-aimed_secs = [i for i in range(20,30)]
-
-
-done_links = []
-done_chnks = get_done_chunks()
-
-for l in list(links.keys()):
-    print(l)
-    
-
-    # l.click()
-
-dl_dir = '/home/johannes/mlhd/dl/'
-gnr_dir = '/home/johannes/mlhd/'
-
+# done_chnks = get_done_chunks()
 # sebgs = "021"
-
-
-for vlu in links.keys():
-    if vlu in done_chnks:
-        pass
-    else:
-        print(vlu)
-    
-        links[vlu].click()
-
-        time.sleep(5)
-        dl_fin_chck(sebgs)
-        fl_prep(sebgs)
 
 
 def get_done_chunks():
@@ -94,24 +85,44 @@ def dl_fin_chck(sebgs):
     fl_nm_part = dl_dir +'MLHD_' + sebgs + '.tar.part'
     
     dir_files = os.listdir(dl_dir)
+    no_goods = 0
 
+    fl_sz_old = 0
+    
     while True: 
         try:
+            print('getting filesize of part' + fl_nm_part)
             fl_sz = os.path.getsize(fl_nm_part)
             
+            if fl_sz_old == fl_sz:
+                no_goods+=1
+            else:
+                no_goods = 0
+            print(no_goods)
+                
+            fl_sz_old = fl_sz
+
+            if no_goods == 3:
+                print('ABORT')
+                res = 'fail'
+                break
+        
         except:
-            print('part file not there')
+
             try:
                 fl_sz_fnl = os.path.getsize(fl_nm)
-                print('final file here')
+                print(fl_nm + ' final file here')
+                res = 'success'
                 break
             
             except:
-                pass
+                res = 'someting wong'
+                break
                 
         time.sleep(5)
         
-    print(sebgs + ' finished')
+    return(res)
+    # print(sebgs + ' finished')
     
     
 def fl_prep(sebgs):
@@ -137,18 +148,11 @@ def fl_prep(sebgs):
 def get_US_logs(log_dir):
 
     """deletes all the non-us files, moves the remaining ones to US folder"""
-    
+
     files1=os.listdir(log_dir)
     log_files = [i for i in files1 if i.endswith('.txt.gz')]
     log_files_ids = [i[0:36] for i in log_files]
 
-    # creation no longer needed
-    # cri_dict_rows = client.execute('select uuid, country from usr_info')
-    # cri_dict = {}
-    # for i in cri_dict_rows:
-    #     cri_dict[i[0]] = i[1]
-    # with open(gnr_dir + 'cri_dict.json' , 'w') as fo:
-    #     json.dump(cri_dict, fo)
 
     with open(gnr_dir + 'cri_dict.json', 'r') as fi:
         cri_dict = json.load(fi)
@@ -161,6 +165,7 @@ def get_US_logs(log_dir):
         try:
             cri = cri_dict[i]
             if cri == 'US':
+
                 pass
             else:
                 del_str = 'cd ' + log_dir + ' && rm ' + i + '.txt.gz'
@@ -179,20 +184,94 @@ def get_US_logs(log_dir):
     os.system(mv_str2)
 
     # return(US_logs)
+def cls_dl_win():
+    parentWindow= driver.window_handles[0]
+    all_windows = driver.window_handles
+    non_main = [i for i in all_windows if i != parentWindow]
+    for i in non_main:
+        driver.switch_to_window(i)
+        time.sleep(3)
+        driver.close()
+    driver.switch_to_window(parentWindow)
+    
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start', help='start chunk id')
+    parser.add_argument('end', help='end chunk id')
+
+    args = parser.parse_args()
+    start = int(args.start)
+    end = int(args.end)
+
+    dl_dir = '/home/johannes/mlhd/dl/'
+    gnr_dir = '/home/johannes/mlhd/'
+    
+    driver, links = setup(start, end)
+    print('setup done')
+
+    # links = get_links(driver, start,end)
+
+    for vlu in links.keys():
+        done_chnks = get_done_chunks()
+        if vlu in done_chnks:
+            pass
+        else:
+            print(vlu)
+
+            print(links[vlu])
+            
+            links[vlu].click()
+            # close download window
+            cls_dl_win()
+
+            time.sleep(2)
+
+            # ttl_no_goods = 0 
+            while True:
+                res = dl_fin_chck(vlu)
+                if res == 'success':
+                    break
+                
+                else:
+                    clean_str = 'rm ' + dl_dir + '*'
+                    os.system(clean_str)
+                    driver.quit()                    
+                    while True:
+                        try:
+                            driver, links2 = setup(start, end)
+                            break
+                        except:
+                            print('go to sleep')
+                            driver.quit()
+                            time.sleep(40)
+                            pass
+
+                    links2[vlu].click()
+                    # close download window
+                    cls_dl_win()
+                    
+
+                    # ttl_no_goods+=1
+                    # if ttl_no_goods ==3:
+                    #     driver, links = setup(start, end)
+                    
+                    
+            fl_prep(vlu)
 
 
 
 
-
-# testfile = '/home/johannes/mlhd/11/2b1a9c83-231b-40cd-be09-a11ea381fd3b.txt.gz'
-
-# slines = []
-# with gzip.open(testfile,'rt') as f:
-#     for line in f:
-#         slines.append(line)
-        
+# import gzip
 
 
 
-
-import gzip
+# * scrap
+# ** creation of usr country dict, no longer needed
+# cri_dict_rows = client.execute('select uuid, country from usr_info')
+# cri_dict = {}
+# for i in cri_dict_rows:
+#     cri_dict[i[0]] = i[1]
+# with open(gnr_dir + 'cri_dict.json' , 'w') as fo:
+#     json.dump(cri_dict, fo)
