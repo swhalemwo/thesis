95ce4f52b87ef23d3fdf6220eb38c700b4494dd6
johannes ae <jojo.ae@hotmail.de>
2019-09-17 11:32:37 +0200

    model combinations, add nbr genres to PCH table, finalize output tables

--- anls/try1/anlsR/robust.R
+++ anls/try1/anlsR/robust.R
@@ -5,6 +5,9 @@ library(psych)
 library(xtable)
 library(Hmisc)
 library(collections)
+library(fBasics)
+library(gmodels)
+library(plotrix)
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
 len = length
@@ -27,7 +30,7 @@ texreg_cleaner <- function(infile){
 
 ## extraction functions
 
-phreg_mdlr <- function(mdl, d, imprvmnt){
+phreg_mdlr <- function(mdl, d, imprvmnt, nbr_gnrs){
     vrbls <- mdl$covars
     vrbl_names = unlist(lapply(vrbls, function(x){d$get(x)}))
     coefs <- mdl$coefficients
@@ -35,11 +38,11 @@ phreg_mdlr <- function(mdl, d, imprvmnt){
     ses <- sqrt(vars)
     pvlus <- 2*pnorm(abs(coefs/ses), lower.tail = FALSE)
     
-    gof.vlus <- c(mdl$events, mdl$ttr, max(mdl$loglik), mdl$df)
+    gof.vlus <- c(nbr_gnrs, mdl$events, mdl$ttr, max(mdl$loglik), mdl$df)
     ## maybe add  model improvement? could do manually later
     ## anova doesn't like 
                   
-    gof.names <- c('events', 'genre-timeperiods', 'Log-likelihood', 'df')
+    gof.names <- c('genres', 'events', 'genre-timeperiods', 'Log-likelihood', 'df')
     
 
     res <- createTexreg(coef.names = vrbl_names,
@@ -53,10 +56,6 @@ phreg_mdlr <- function(mdl, d, imprvmnt){
 }
 
 ## * data
-res_dirs <- c(
-    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/harsh_narrow/',
-    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/harsh_wide/'
-)
 
 
 dt_prpr <- function(res_dir){
@@ -72,7 +71,15 @@ dt_prpr <- function(res_dir){
         dfc <- rbind(dfc, dfx)
     }
 
-    EMPT_END <- 4
+    harsh_coef <- substr(res_dir, nchar(res_dir)-4, nchar(res_dir)-1)
+
+    empt_endr <- Dict$new(list(
+                          '1.25' = 2,
+                          '_1.0' = 3,
+                          '0.75' = 4))
+
+
+    EMPT_END <- empt_endr$get(harsh_coef)
     dfc$X <- as.factor(dfc$X)
     dfc <- dfc[order(dfc$X, dfc$tp_id),]
 
@@ -117,8 +124,8 @@ dt_prpr <- function(res_dir){
 }
 
 dt_fnlzr <- function(dfc2, ctrl_vars){
-    dfc2$inftns <- log(dfc2$prnt3_dvrg)
-    dfc2$inftns_sqrd <- log(dfc2$prnt3_dvrg)^2
+    dfc2$inftns <- log(dfc2$prnt3_dvrg+0.01)
+    dfc2$inftns_sqrd <- log(dfc2$prnt3_dvrg+0.01)^2
 
     dfc2$disctns <- log(dfc2$cohrt_mean_non_inf_wtd+0.01)
 
@@ -139,6 +146,8 @@ dt_fnlzr <- function(dfc2, ctrl_vars){
 
     dfc2$avg_weight_rel_wtd <- dfc2$avg_weight_rel_wtd
     ## ctrl_vars <- c('avg_weight_rel_wtd', 'cos_sims_mean_wtd', 'gnr_gini', 'avg_age', 'sz', 'new_rlss', 'gnr_age2')
+    dfc2$mean_prnt_kld_wtd <- log(dfc2$mean_prnt_kld_wtd+0.01)
+
 
     dfc2$tp_id2 <- dfc2$tp_id+1
 
@@ -158,12 +167,15 @@ dt_fnlzr <- function(dfc2, ctrl_vars){
 
 mdl_rnr <- function(dfc3, ctrl_vars){
 
+    
     d <- Dict$new(list(
                   "inftns" = "Informativeness", 
                   "inftns_sqrd" = "Informativeness\\textsuperscript{2}",
                   "disctns" = "Distinctiveness", 
                   "dens_vol" = "Density (vol)",
                   "dens_len" = "Density (len)",
+                  "mean_prnt_kld_wtd" = "Parent similarity",
+                  "cohrt_ovlp" = "Cohort Overlap",
                   "dens_vol_sqrd" = "Density\\textsuperscript{2} (vol)",
                   "dens_len_sqrd" = "Density\\textsuperscript{2} (len)", 
                   "cohrt_dom" = "Cohort Domination", 
@@ -178,18 +190,60 @@ mdl_rnr <- function(dfc3, ctrl_vars){
                   "new_rlss" = "New Releases", 
                   "gnr_age2" = "Genre Age"))
 
+    inf_vars <- c('inftns', 'inftns_sqrd', 'disctns')
+    dens_vars <- c('dens_vol', 'dens_len', 'dens_vol_sqrd', 'dens_len_sqrd', 'cohrt_rel_sz', 'leg')
+
+    all_vars <- c(inf_vars, dens_vars, ctrl_vars)
+
+    tbl <- t(basicStats(dfc3[,all_vars])[c("Mean", "Median", "Minimum", "Maximum"),])
+    colnames(tbl) <- c("Mean", "Median", "Min.", "Max.")
+    tbl <- round(tbl, 3)
+    vrbl_names = unlist(lapply(all_vars, function(x){d$get(x)}))
+    rownames(tbl) <- vrbl_names
+
+    cor_tbl <- cor(dfc3[,all_vars])
+    cor_tbl[upper.tri(cor_tbl, diag=TRUE)] <- NA
+    rownames(cor_tbl) <- vrbl_names
+    colnames(cor_tbl) <- vrbl_names
+    cor_tbl <- round(cor_tbl, 3)
+
+    cor_tbl2 <- cor_tbl[, -ncol(cor_tbl)]
+
+
+    rnms2 <- unlist(lapply(seq(1:nrow(cor_tbl2)),
+                           function(x){paste0('(', as.character(x), ') ', rownames(cor_tbl2)[x])}))
+    cnms2 <- unlist(lapply(seq(1:ncol(cor_tbl2)), function(x){paste0('(', as.character(x), ')')}))
+
+    rownames(cor_tbl2) <- rnms2
+    colnames(cor_tbl2) <- cnms2
+
+
+
+
     dv <- 'Surv(tp_id, tp_id2, event)'
     ctrl_vars_cbnd <- paste(ctrl_vars, collapse = ' + ')
     v_inf5 <- paste(c(ctrl_vars_cbnd, 'inftns', 'inftns_sqrd', 'disctns', 'dens_vol', 'dens_vol_sqrd', 'dens_len', 'dens_len_sqrd', 'leg'), collapse = ' + ')
     f_inf5 <- as.formula(paste(c(dv, v_inf5) , collapse = ' ~ ' ))
     fit_inf5 <- phreg(f_inf5, data=dfc3, cuts = seq(min(dfc3$tp_id)+1,max(dfc3$tp_id)),dist = 'pch')
-    res_inf5 <- phreg_mdlr(fit_inf5, d, None)
+    res_inf5 <- phreg_mdlr(fit_inf5, d, None, len(unique(dfc3$X)))
 
-    return(res_inf5)
+ 
+    ## fitx_ph <- coxph(f_inf5, data = dfc3)
+    ## print(screenreg(fitx5))
+    ## print(cox.zph(fitx5))
+
+    frail_vars <- paste(c(v_inf5, 'frailty(X)'), collapse = ' + ')
+    frail_f <- as.formula(paste(c(dv, frail_vars), collapse = ' ~ '))
+    fit_frailty <- coxph(frail_f, data = dfc3)
+
+
+    return(list(res_inf5, fit_frailty, tbl, cor_tbl2))
 }
 
 
 proc_func <- function(res_dir){
+
+    print(res_dir)
 
     ctrl_vars <- c('avg_weight_rel_wtd', 'cos_sims_mean_wtd', 'gnr_gini', 'avg_age', 'sz', 'new_rlss', 'gnr_age2')
     dfc2 <- dt_prpr(res_dir)
@@ -201,10 +255,185 @@ proc_func <- function(res_dir){
     return(res)
 }
 
+## * actually running
+
+res_dirs <- c(
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_0.75_tp_0.75/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.0_tp_0.75/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.25_tp_0.75/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_0.75_tp_1.0/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.0_tp_1.0/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.25_tp_1.0/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_0.75_tp_1.25/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.0_tp_1.25/',
+    '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust_v2/harsh_1.25_tp_1.25/'
+)
+
+
+
 all_res <- lapply(res_dirs, proc_func)
 
-screenreg(all_res)
+pch_res <- lapply(all_res, function(x){x[[1]]})
+frail_res <- lapply(all_res, function(x){x[[2]]})
+sum_tbls  <- lapply(all_res, function(x){x[[3]]})
+cor_tbls  <- lapply(all_res, function(x){x[[4]]})
 
 
-# seems that 
+## * summary stats
+
+## ** descriptives
+arr <- array( unlist(sum_tbls) , c(16,4,9))
+arr_means <- apply(arr, 1:2, mean)
+arr_ses <- apply(arr, 1:2, std.error)
+
+sum_trs = list()
+
+for (i in 1:4){
+    print(i)
+    sum_tr <- createTexreg(coef = arr_means[,i],
+                           coef.names = rownames(sum_tbls[[1]]),
+                           ci.up = arr_means[,i] + 1.96 * arr_ses[,i],
+                           ci.low = arr_means[,i] - 1.96 * arr_ses[,i])
+    
+    ## sum_trs <- c(sum_trs, sum_tr)
+    sum_trs[i] <- sum_tr
+}
+
+p_lists = lapply(1:4, function(x){rep(0.5, 16)})
+se_lists = lapply(1:4, function(x){rep(10, 16)})
+    
+texreg(sum_trs, custom.model.names = c('Mean', 'Median', 'Min.', 'Max.'),
+       ci.test = NULL,
+       caption = 'Summary Statistics',
+       label = 'summaries',
+       custom.note = '95 \\% CI of the 9 models in square brackets',       
+       file = '~/Dropbox/gsss/thesis/text/tables/summaries_mult.tex', 
+       use.packages = FALSE)
+
+texreg_cleaner('~/Dropbox/gsss/thesis/text/tables/summaries_mult.tex')     
+
+## ** correlation
+arr_cor <- array(unlist(cor_tbls), c(16, 15, 9))
+arr_cor_means <- apply(arr_cor, 1:2, mean)
+arr_cor_ses <- apply(arr_cor, 1:2, std.error)
+
+cor_trs = list()
+
+for (i in 1:15){
+    print(i)
+    cor_tr <- createTexreg(coef = arr_cor_means[,i],
+                           coef.names = rownames(cor_tbls[[1]]),
+                           ci.up = arr_cor_means[,i] + 1.96 * arr_cor_ses[,i],
+                           ci.low = arr_cor_means[,i] - 1.96 * arr_cor_ses[,i])
+    
+    ## sum_trs <- c(sum_trs, sum_tr)
+    cor_trs[i] <- cor_tr
+}
+
+texreg(cor_trs, ci.test = NULL, custom.model.names = colnames(cor_tbls[[1]]),
+       sideways = TRUE, file = '~/Dropbox/gsss/thesis/text/tables/cors_mult.tex',
+       use.packages = FALSE, scalebox = 0.65,
+       caption = 'Correlation Table',
+       label = 'cor_tbl',
+       custom.note = '95 \\% CI of the 9 models in square brackets'
+       )
+
+texreg_cleaner('~/Dropbox/gsss/thesis/text/tables/cors_mult.tex')
+
+
+
+
+
+screenreg(pch_res)
+## screenreg(ph_res)
+screenreg(frail_res)
+
+
+## * meta analysis
+
+## https://stats.stackexchange.com/questions/5586/average-effect-of-coefficients-across-multiple-linear-models
+## The standard error of the average then will be the square root from the average of the squares of the standard errors.
+
+
+## lapply(frail_res, function(x){x[[1]]@'se(coef)'^2})
+
+ses <- lapply(pch_res, function(x){x@se^2})
+se_mat <- matrix(unlist(ses), ncol = 9)
+se_avg <- sqrt(rowMeans(se_mat))
+
+coefs <- lapply(pch_res, function(x){x@coef})
+coefs_mean <- rowMeans(matrix(unlist(coefs), ncol = 9))
+
+pvlus <- 2*pnorm(abs(coefs_mean/se_avg), lower.tail = FALSE)
+
+meta_mdl <- createTexreg(coef.names = pch_res[1][[1]]@coef.names, coef = coefs_mean, se = se_avg, pvalues = pvlus)
+texreg(meta_mdl, custom.model.names = c('Genre Abandonment'),
+       dcolumn = TRUE, use.packages = FALSE,
+       caption = "Summary Model",
+       label = 'res_sum',
+       custom.note = '\\parbox{.4\\linewidth}{\\vspace{2pt}%stars. \\\\Coefficients (averaged across models) are logged multiplicative effects on the hazard of abandonment. Standard errors in parentheses}.',
+       file = '~/Dropbox/gsss/thesis/text/tables/res_sum.tex',
+       table = FALSE)
+
+
+
+## * export
+
+## ** pch
+
+mod_names <- rep(c('lenient', 'medium', 'strict'), 3)
+
+texreg3(pch_res,
+        custom.model.names = mod_names,
+        col.groups = list('12 weeks' = 1:3, '16 weeks' = 4:6, '20 weeks' = 7:9) ,
+        file = '~/Dropbox/gsss/thesis/text/tables/pch_res.tex',
+        caption = 'Piecewise Constant Exponential Hazard Models',
+        label = 'pch',
+        dcolumn = TRUE,
+        sideways = TRUE,
+        use.pa tckages = FALSE)
+
+## texreg_cleaner('~/Dropbox/gsss/thesis/text/tables/mult_test.tex')
+
+## ** frailty
+mod_names <- rep(c('lenient', 'medium', 'strict'), 3)
+
+texreg3(frail_res,
+        custom.model.names = mod_names,
+        col.groups = list('12 weeks' = 1:3, '16 weeks' = 4:6, '20 weeks' = 7:9) ,
+        file = '~/Dropbox/gsss/thesis/text/tables/frail_res.tex',
+        caption = 'Proportional Hazard Models with individual frailty',
+        label = 'frailty',
+        dcolumn = TRUE,
+        custom.coef.names = pch_res[1][[1]]@coef.names,
+        sideways = TRUE,
+        use.packages = FALSE)
+
+
+
+
+## * poisson test
+dfc3$tp_id3 <- as.factor(dfc3$tp_id)
+fitx <- glm(event ~ avg_weight_rel_wtd + cos_sims_mean_wtd + 
+    gnr_gini + avg_age + sz + new_rlss + gnr_age2 + inftns + 
+    inftns_sqrd + disctns + dens_vol + dens_vol_sqrd + dens_len + 
+    dens_len_sqrd + leg +tp_id3, data = dfc3, family = 'poisson')
+
+
+for (i in ctrl_vars){
+    print(i)
+    cor(dfc2$mean_prnt_kld_wtd, dfc2[,i])
+    }
+
+lapply(ctrl_vars, function(x){cor(dfc2$mean_prnt_kld_wtd, dfc3[,x])})
+
+
+
+## * debug shit
+
+debugx <- read.csv('/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust/harsh_0.75_tp_0.75/debug.csv', header = FALSE)
+## head(debugx)
+
+debugx2 <- debugx[debugx$V2 < 15,]
+write.table(debugx2, '/home/johannes/Dropbox/gsss/thesis/anls/try1/results/robust/harsh_0.75_tp_0.75/debug2.csv', row.names=FALSE, quote = FALSE, col.names = FALSE, sep = ',')
 
